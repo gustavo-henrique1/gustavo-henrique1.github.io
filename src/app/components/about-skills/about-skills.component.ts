68edb149ea92ddd485d734eb0cf1b48f71f221d5
gustavo-henrique1 <gustavohenrique.fortes@gmail.com>
2025-07-16 17:45:07 -0300

    feat: complementando a seção about

--- src/app/components/about-skills/about-skills.component.ts
+++ src/app/components/about-skills/about-skills.component.ts
@@ -1,36 +1,21 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about-skills',
   templateUrl: './about-skills.component.html',
   styleUrl: './about-skills.component.scss',
-  animations: [
-    trigger('fadeInUp', [
-      transition('hidden => visible', [
-        style({ opacity: 0, transform: 'translateY(20px)' }),
-        animate(
-          '600ms ease-out',
-          style({ opacity: 1, transform: 'translateY(0)' })
-        ),
-      ]),
-    ]),
-  ],
 })
 export class AboutSkillsComponent implements OnInit {
   skills = [
-    { name: 'UX DESIGN', color: '#fca652' },
-    { name: 'BRANDING', color: '#58b4f0' },
-    { name: 'WEB DESIGN', color: '#34d399' },
-    { name: 'FRAMER', color: '#a78bfa' },
-    { name: 'STRATEGY', color: '#f87171' },
-    { name: 'USER RESEARCH', color: '#ec4899' },
+    { name: 'UX DESIGN' },
+    { name: 'ANÁLISE' },
+    { name: 'WEB DESIGN' },
+    { name: 'FRONT-END' },
+    { name: 'ESTRATÉGIA' },
+    { name: 'UI DESIGN' },
+    { name: 'INTERFACE' },
+    { name: 'SCRUM' },
+    { name: 'USUÁRIO' },
   ];
 
   constructor() {}
