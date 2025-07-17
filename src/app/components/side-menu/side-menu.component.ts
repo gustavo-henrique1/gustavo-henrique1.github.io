aa68202e1394c1f91a073e63cead9452093be8ff
gustavo-henrique1 <gustavohenrique.fortes@gmail.com>
2025-07-17 20:30:39 -0300

    fix: correção de link no menu

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -4,21 +4,21 @@ import {
   Component,
   EventEmitter,
   Output,
-} from '@angular/core';
-import { ThemeService } from 'src/app/services/theme.service';
+} from "@angular/core";
+import { ThemeService } from "src/app/services/theme.service";
 
 @Component({
-  selector: 'app-side-menu',
-  templateUrl: './side-menu.component.html',
-  styleUrl: './side-menu.component.scss',
+  selector: "app-side-menu",
+  templateUrl: "./side-menu.component.html",
+  styleUrl: "./side-menu.component.scss",
 })
 export class SideMenuComponent implements AfterViewInit {
   @Output() darkModeChanged = new EventEmitter<boolean>();
 
-  activeSection: string = 'home';
+  activeSection: string = "home";
   darkMode = false;
-  darkModeIcon = 'moon_stars';
-  srcIcon = '';
+  darkModeIcon = "moon_stars";
+  srcIcon = "";
 
   constructor(
     private cdr: ChangeDetectorRef,
@@ -26,7 +26,7 @@ export class SideMenuComponent implements AfterViewInit {
   ) {}
 
   ngAfterViewInit() {
-    const sections = document.querySelectorAll('section');
+    const sections = document.querySelectorAll("section");
 
     const observer = new IntersectionObserver(
       (entries) => {
@@ -50,8 +50,8 @@ export class SideMenuComponent implements AfterViewInit {
       }
     });
 
-    this.darkMode = this.getCookie('darkMode') === 'true';
-    document.body.classList.toggle('dark-mode', this.darkMode);
+    this.darkMode = this.getCookie("darkMode") === "true";
+    document.body.classList.toggle("dark-mode", this.darkMode);
 
     this.togleIcons();
 
@@ -61,7 +61,7 @@ export class SideMenuComponent implements AfterViewInit {
   scrollToSection(sectionId: string) {
     const element = document.getElementById(sectionId);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
 
       setTimeout(() => {
         this.activeSection = sectionId;
@@ -71,8 +71,8 @@ export class SideMenuComponent implements AfterViewInit {
 
   toggleDarkMode() {
     this.darkMode = !this.darkMode;
-    document.body.classList.toggle('dark-mode', this.darkMode);
-    this.setCookie('darkMode', this.darkMode ? 'true' : 'false', 30);
+    document.body.classList.toggle("dark-mode", this.darkMode);
+    this.setCookie("darkMode", this.darkMode ? "true" : "false", 30);
     this.themeService.setDarkMode(this.darkMode);
     this.togleIcons();
   }
@@ -80,25 +80,25 @@ export class SideMenuComponent implements AfterViewInit {
   setCookie(name: string, value: string, days: number) {
     const date = new Date();
     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    const expires = 'expires=' + date.toUTCString();
-    document.cookie = name + '=' + value + ';' + expires + ';path=/';
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
   }
 
   getCookie(name: string): string {
-    const nameEQ = name + '=';
-    const ca = document.cookie.split(';');
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(";");
     for (let i = 0; i < ca.length; i++) {
       let c = ca[i];
-      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      while (c.charAt(0) == " ") c = c.substring(1, c.length);
       if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
-    return '';
+    return "";
   }
 
   togleIcons() {
-    this.darkModeIcon = this.darkMode ? 'clear_day' : 'moon_stars';
+    this.darkModeIcon = this.darkMode ? "clear_day" : "moon_stars";
     this.srcIcon = this.darkMode
-      ? 'assets/images/icon-logo-dark.png'
-      : 'assets/images/icon-logo.png';
+      ? "assets/images/icon-logo-dark.png"
+      : "assets/images/icon-logo.png";
   }
 }
